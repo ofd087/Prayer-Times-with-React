65e849cc68081b89d981a90c08100d558841c45e
ofd087 <113826922+ofd087@users.noreply.github.com>
2025-10-15 14:17:25 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Prayer-Times-with-React/',
   server: {
     port: 3000,
   },
-  base: '/Prayer-Times-with-React/',
 });
